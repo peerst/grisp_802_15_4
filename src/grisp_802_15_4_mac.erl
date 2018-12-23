77a3046bc859bb163ef126a14f2f0c8143cbc70f
zofpolkowska <zofpolkowska@gmail.com>
2018-12-23 21:59:44 +0100

    Transmition and reception of acknowledged packet. Bugged, but works

--- src/grisp_802_15_4_mac.erl
+++ src/grisp_802_15_4_mac.erl
@@ -9,23 +9,28 @@
                               write/4]).
 -export([force_transmit_mode/0, force_receive_mode/0]).
 -export([load_tx_fifo/4, load_frame_control/1, load_addr_fields/1, load_msdu/3]).
--export([transmit/0]).
+-export([transmit/1]).
 -export([read_tx_fifo/0]).
 -export([read_addr_fields/3, read_msdu/3]).
 -export([read_rx_fifo/0]).
 
 force_transmit_mode() ->
+    write(short, ?RFCTL, 16#04),
     write(short, ?RFCTL, 16#02).
 
 force_receive_mode() ->
+    write(short, ?RFCTL, 16#04),
     write(short, ?RFCTL, 16#01).
 
-transmit() ->
+transmit(_Frame = #frame{ mpdu = #mpdu{ mhr = #mhr{ fc = #fc{ ack = 1 }}}}) ->
+    grisp_802_15_4_reg:write(short, 16#1B, 5);
+transmit(_Frame) ->
     grisp_802_15_4_reg:set_bit(short, 16#1B, 0).
 
+%% Acknowledged frame has frame length 0 (?!?), MSDULength has to be passed as part of payload
+%% Payload = << MSDULength:8, Rest/binary >>
 load_tx_fifo(FrameControlMap, Sequence, DestinationMap, MSDU) ->
     MHRLength = 19,
-    SN = 16#FA,
     write(long, ?TX_FIFO, 19),
     write(long, ?TX_FIFO + 1, 19 + size(MSDU)),
     FC = load_frame_control(FrameControlMap),
@@ -88,30 +93,44 @@ read_tx_fifo() ->
                  msdu = read(long, ?TX_FIFO + 21, <<>>, FrameLength - HeaderLength)},
    #frame{ length = FrameLength, mpdu = MPDU }.
 
+recv_frame_length() ->
+    read_integer(long, ?RX_FIFO).
+
 read_rx_fifo() ->
-   FrameLength = read_integer(long, ?RX_FIFO),
    <<T:3, Sec:1, Pen:1, Ack:1, Intra:1, _:1>> = read(long, ?RX_FIFO + 1),
    <<_:2, DMode:2, _:2, SMode:2>> = read(long, ?RX_FIFO + 2),
-   <<S:8>> = read(long, ?RX_FIFO + 3),
    Type = translate_t(T),
+   read_rx_fifo(#fc{ t = Type,
+                     sec = Sec,
+                     pen = Pen,
+                     ack = Ack,
+                     intra = Intra,
+                     dst_m = DMode,
+                     src_m = SMode }).
+read_rx_fifo(FC = #fc{ t = data, ack = Ack, intra = Intra, dst_m = DMode, src_m = SMode }) ->
    MHRLength  = mhr_length(Intra, DMode, SMode),
-   MSDULength = FrameLength - MHRLength - 5,
-   FCS = read(long, ?RX_FIFO + FrameLength - 2, <<>>, 2),
-   LQI = read(long, ?RX_FIFO + FrameLength),
-   MPDU = #mpdu{ mhr = #mhr{ fc = #fc{ t = Type,
-                                       sec = Sec,
-                                       pen = Pen,
-                                       ack = Ack,
-                                       intra = Intra,
-                                       dst_m = DMode,
-                                       src_m = SMode },
+   <<S:8>> = read(long, ?RX_FIFO + 3),
+   {FrameLength, MSDULength} = case recv_frame_length() of
+                                   0 ->
+                                       M = read_integer(long, ?RX_FIFO + MHRLength + 1),
+                                       {M + MHRLength + 2, M};
+                                   N ->
+                                       {N, N - MHRLength - 2}
+                               end,
+%%   MSDULength = FrameLength - MHRLength - 2,
+   FCS = read(long, ?RX_FIFO + FrameLength - 1, <<>>, 2),
+   LQI = read(long, ?RX_FIFO + FrameLength + 1),
+   MPDU = #mpdu{ mhr = #mhr{ fc = FC,
                              sn = S,
                              addr_fields = read_addr_fields(Intra, DMode, SMode)},
-                 msdu = read_msdu(Type, MHRLength, MSDULength)},
+                 msdu = read_msdu(data, MHRLength, MSDULength)},
    #frame{ length = FrameLength,
            mpdu = MPDU,
-           fcs = grisp_802_15_4_reg:decode(FCS,16),
-           lqi = grisp_802_15_4_reg:decode(LQI, 16) }.
+           fcs = grisp_802_15_4_reg:decode(FCS,10),
+           lqi = grisp_802_15_4_reg:decode(LQI, 10)
+         };
+read_rx_fifo(_FC = #fc{ t = Type }) ->
+    logger:error("MPDU of type ~p not supported", [Type]).
 
 translate_t(0) ->
     beacon;
@@ -136,7 +155,7 @@ read_addr_fields(1, 3, 3) ->
                   src = #addr{ eadr = Src } }.
 
 read_msdu(data, MHRLength, MSDULength) ->
-    read(long, ?RX_FIFO + MHRLength, <<>>, MSDULength).
+    read(long, ?RX_FIFO + MHRLength + 1, <<>>, MSDULength).
 
 mhr_length(1, 3, 3) ->
-    16.
+    19.
